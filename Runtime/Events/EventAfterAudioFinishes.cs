7355ff26ec5366702760cd9e950f3af1a4c172dc
Brennan Hatton - MSImini <brennan@brennanhatton.com>
2023-02-02 08:32:19 +1100

    Event after audio finishes

---
+++ Runtime/Events/EventAfterAudioFinishes.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace BrennanHatton.UnityTools
+{
+	
+	public class EventAfterAudioFinishes : MonoBehaviour
+	{
+		
+		public UnityEvent onFinish = new UnityEvent();
+	    
+		public void RunWhenFinished(AudioSource audioSource)
+		{
+			//Debug.Log("RunWhenFinished");
+			StartCoroutine(runWhenFinished(audioSource));
+		}
+		
+		IEnumerator runWhenFinished(AudioSource audioSource)
+		{
+			//Debug.Log("runWhenFinished");
+			//float time = 0;
+			while(audioSource.isPlaying)
+			{
+				yield return new WaitForEndOfFrame();
+				
+				/*if(time > 120)
+				{
+					Debug.Log("Waiting for autio");
+					time = 0;
+				}
+				time++;*/
+
+			}
+			//Debug.Log("onFinishInvoke");
+			
+			onFinish.Invoke();
+			
+			yield return null;
+		}
+	}
+	
+}
