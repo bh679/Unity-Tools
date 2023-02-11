d65b512aa0867e2490513933a264e908954dc515
Brennan Hatton - MSImini <brennan@brennanhatton.com>
2023-02-12 00:47:39 +1100

    GPT3 Tools

---
+++ Runtime/OpenAI/PromptToText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace BrennanHatton.AI
+{
+	public class PromptToText : MonoBehaviour
+	{
+		public TMP_Text promptText, responseText;
+		public GPT3 gpt;
+		int length = 0;
+		
+		void Reset()
+		{
+			promptText = this.GetComponent<TMP_Text>();
+		}
+		
+		// Update is called once per frame
+		void Update()
+		{
+			if(length != gpt.interactions.Count)
+			{
+				promptText.text = gpt.interactions[gpt.interactions.Count-1].requestData.prompt;
+				responseText.text = gpt.interactions[gpt.interactions.Count-1].GeneratedText;
+				length = gpt.interactions.Count;
+			}
+		}
+	}
+}
