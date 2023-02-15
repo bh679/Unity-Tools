6631bc37db0b23d7fbf33eced5be2cffa975f219
Brennan Hatton - MSImini <brennan@brennanhatton.com>
2023-02-15 20:10:54 +1100

    Some UI Tools (used by OpenAI UI Tools)

---
+++ Runtime/UI/InputRandomText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace BrennanHatton
+{
+
+	public class InputRandomText : MonoBehaviour
+	{
+		public TMP_InputField input;
+		public string[] text;
+		public bool onStart = true;
+		
+		void Reset()
+		{
+			input = this.GetComponent<TMP_InputField>();
+		}
+		
+		
+	    // Start is called before the first frame update
+	    void Start()
+	    {
+		    if(onStart)
+			    SetRandom();
+	    }
+	    
+		public void SetRandom()
+		{
+			input.text = text[Random.Range(0,text.Length)];
+		}
+	}
+
+}
