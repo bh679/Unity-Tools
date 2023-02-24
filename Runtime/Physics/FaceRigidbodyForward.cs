d1cfe0c247062e2aecfa3f66e200089168f78334
Brennan Hatton <brennan@brennanhatton.com>
2023-02-24 17:51:24 +1100

    Physics scripts, not used in, but from ER metaverse classrooms

---
+++ Runtime/Physics/FaceRigidbodyForward.cs
@@ -0,0 +1,35 @@
+﻿/*
+Brennan Hatton
+Has this game object face the relative direction of the target rigibody velocity
+2021
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrennanHatton.UnityTools
+{
+
+	public class FaceRigidbodyForward : MonoBehaviour
+	{
+		public Rigidbody rb;
+		
+		void Reset(){
+			rb = this.GetComponent<Rigidbody>();
+		}
+		
+	    // Start is called before the first frame update
+	    void Start()
+		{
+			if(rb == null)
+				rb = this.GetComponent<Rigidbody>();
+	    }
+	
+	    // Update is called once per frame
+	    void Update()
+	    {
+		    this.transform.forward = rb.velocity.normalized;
+	    }
+	}
+
+}
---
+++ Runtime/Physics/FollowPhsyics.cs
@@ -0,0 +1,38 @@
+﻿/*
+Brennan Hatton
+Has rigibody copy position of target transform;
+2022
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrennanHatton.UnityTools
+{
+
+	[RequireComponent(typeof(Rigidbody))]
+	public class FollowPhsyics : MonoBehaviour
+	{
+		public Transform target;
+		Rigidbody rb;
+		
+	    // Start is called before the first frame update
+	    void Start()
+	    {
+		    rb = this.GetComponent<Rigidbody>();
+	    }
+	
+	    // Update is called once per frame
+	    void Update()
+	    {
+		    rb.MovePosition(target.transform.position);
+	    }
+	    
+		void LateUpdate()
+		{
+			target.SetParent(this.transform);
+			target.localPosition = Vector3.zero;
+		}
+	}
+
+}
