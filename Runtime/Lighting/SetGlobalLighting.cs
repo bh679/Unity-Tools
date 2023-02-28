3c08f0a01ce863c14a5240bdf1826018fbcf1753
Brennan Hatton <brennan@brennanhatton.com>
2023-03-01 09:39:22 +1100

    Migrating over scripts from Metaverse - SetGlobalLighting and TransformScale

---
+++ Runtime/Lighting/SetGlobalLighting.cs
@@ -0,0 +1,105 @@
+﻿//BrennanHatton
+//2018
+//For changing lighting settings realtime in game
+
+using UnityEngine;
+
+
+namespace BrennanHatton.UnityTools
+{
+	
+	[ExecuteInEditMode]
+	public class SetGlobalLighting : MonoBehaviour {
+		
+		public enum FogSettings
+		{
+			None = 0,
+			Linear = 1,
+			Exponential = 2,
+			ExponentialSquared = 3
+		}
+		
+		public bool onStart = false;
+		public bool onGameObjectActive = true;
+		public bool changeAmbientLightSource = true;
+		public UnityEngine.Rendering.AmbientMode ambientLightSource = UnityEngine.Rendering.AmbientMode.Flat;
+		public Color color;
+		
+		public Material skybox;
+		
+		public FogSettings fog = FogSettings.None;
+		public float fogDensity = 0.01f;
+		public float fogstart = 50, fogend = 200f;
+		
+		
+		
+		bool objectActive ;
+		
+		void OnEnable()
+		{
+			if(onGameObjectActive)
+				ChangeLighting();
+		}
+		
+		// Use this for initialization
+		void Start () {
+			objectActive = gameObject.activeInHierarchy;
+			if (onStart)
+				ChangeLighting();
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+			if(onGameObjectActive && gameObject.activeInHierarchy && objectActive == false)
+				ChangeLighting();
+			
+			objectActive = gameObject.activeInHierarchy;
+		}
+		
+		public void ChangeLighting()
+		{
+			if(changeAmbientLightSource)
+			{
+				if(ambientLightSource != RenderSettings.ambientMode)
+					RenderSettings.ambientMode = ambientLightSource;
+				
+				if(RenderSettings.ambientLight != color)
+					RenderSettings.ambientLight = color;
+			}
+			
+			if(skybox!= null)
+				RenderSettings.skybox = skybox;
+			
+			switch((int)fog)
+			{
+				
+			case (int)FogSettings.None:
+				RenderSettings.fog = false;
+				break;
+				
+			case (int)FogSettings.Linear:
+				RenderSettings.fog = true;
+				RenderSettings.fogMode = FogMode.Linear;
+				RenderSettings.fogDensity = fogDensity;
+				RenderSettings.fogStartDistance = fogstart;
+				RenderSettings.fogEndDistance = fogend;
+				break;
+				
+			case (int)FogSettings.Exponential:
+				RenderSettings.fog = true;
+				RenderSettings.fogMode = FogMode.Exponential;
+				RenderSettings.fogDensity = fogDensity;
+				
+				break;
+				
+			case (int)FogSettings.ExponentialSquared:
+				RenderSettings.fog = true;
+				RenderSettings.fogMode = FogMode.ExponentialSquared;
+				RenderSettings.fogDensity = fogDensity;
+				
+				break;
+			}
+		}
+	}
+}
---
+++ Runtime/Transform/TransformScale.cs
@@ -0,0 +1,71 @@
+﻿//Brennan Hatton
+//2020ish
+//This scrip could probable me merged into TransformTools
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrennanHatton.UnityTools
+{
+
+	public class TransformScale : MonoBehaviour
+	{
+		public Vector2 randomScale;
+		
+		public bool onEnable = false;
+		
+		public bool uniform = true;
+		
+		void OnEnable()
+		{
+			if(onEnable)
+			{
+				if(uniform)
+					TransformSetRandomUniformScale();
+				else
+					TransformSetRandomUniformScale();
+			}
+		}
+	    
+		public void ScaleAllRandom()
+		{
+			Vector3 newScale = this.transform.localScale.normalized;
+			
+			newScale.x *= Random.Range(randomScale.x,randomScale.y);
+			newScale.y *= Random.Range(randomScale.x,randomScale.y);
+			newScale.z *= Random.Range(randomScale.x,randomScale.y);
+			
+			this.transform.localScale = newScale;
+		}
+	    
+		public void TransformSetUniformScale(float scale)
+		{
+			this.transform.localScale = this.transform.localScale.normalized*scale;
+		}
+	    
+		public void TransformSetRandomUniformScale()
+		{
+			float scale = Random.Range(randomScale.x,randomScale.y);
+			
+			TransformSetUniformScale(scale);
+		}
+		
+		public void ScaleToZero(float time)
+		{
+			StartCoroutine(scaleToZero(time));
+		}
+		
+		IEnumerator scaleToZero(float time)
+		{
+			Vector3 startScale = transform.localScale;
+			for(int i = 0; i < time*25; i++)
+			{
+				yield return new WaitForSeconds((time/25f) * Time.deltaTime);
+				
+				transform.localScale = startScale*(1-i/(time*25));
+			}
+		}
+	}
+
+}
