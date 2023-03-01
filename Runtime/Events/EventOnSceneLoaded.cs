c02876b3a192f3720910734dc4e8da1397c781c3
Brennan Hatton <brennan@brennanhatton.com>
2023-03-01 12:10:37 +1100

    From Metaverse BrennanHatton/UnityTools folder

--- Runtime/Events/EventOnSceneLoaded.cs
+++ Runtime/Events/EventOnSceneLoaded.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿/*
+Brennan Hatton
+2023-03-01
+Calls Unity event on laod of scene
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,18 +12,56 @@ using UnityEngine.Events;
 namespace BrennanHatton.UnityTools
 {
 
-	public class EventOnSceneLoaded : MonoBehaviour
+	[System.Serializable]
+	public class SceneEvent
 	{
+		public string eventId;
 		public UnityEvent onSceneLoaded;
 		
-		void Awake()
-		{
-			SceneManager.sceneLoaded += OnSceneLoaded;
-		}
-		
-		void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+		public void Invoke()
 		{
 			onSceneLoaded.Invoke();
 		}
 	}
+	
+	public class EventOnSceneLoaded : MonoBehaviour
+	{
+		public SceneEvent onSceneLoaded;
+		static List<SceneEvent> events = new List<SceneEvent>();
+		static bool addedToSceneLoad;
+		
+		void Awake()
+		{
+			if(!IdIsInEvents(onSceneLoaded.eventId))
+			{
+				events.Add(onSceneLoaded);
+			}
+			
+			if(!addedToSceneLoad)
+			{
+				SceneManager.sceneLoaded += OnSceneLoaded;
+				addedToSceneLoad = true;
+			}
+		}
+		
+		static bool IdIsInEvents(string id)
+		{
+			for(int i = 0; i < events.Count; i++)
+			{
+				if(events[i].eventId == id)
+					return false;
+			}
+			
+			return true;
+		}
+		
+		static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+		{
+			for(int i = 0; i < events.Count; i++)
+			{
+				events[i].Invoke();
+			}
+		}
+	}
+
 }
