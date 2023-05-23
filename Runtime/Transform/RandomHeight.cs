31a143f62e7393f68d3cadfe475899f9298ca60f
Brennan Hatton <brennan@brennanhatton.com>
2023-05-23 13:03:01 +0700

    Shuffle

---
+++ Runtime/Transform/RandomHeight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomHeight : MonoBehaviour
+{
+	public float interval = 0.1f;
+	float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	float y = 0;
+    // Update is called once per frame
+    void Update()
+	{
+		if(timer <= 0)
+		{
+			y += Random.Range(-0.25f,0.25f);
+			if(y > 1) y = 1;
+			if(y < 0.1f) y = 0.1f;
+			
+			this.transform.localScale = new Vector3(1,y,1);
+			timer = interval;
+		}
+		timer -= Time.deltaTime;
+    }
+}
---
+++ Runtime/Utilities/Shuffle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrennanHatton.UnityTools
+{
+	public class Shuffle : MonoBehaviour
+	{
+	
+		/// <summary>
+		/// Shuffle the array.
+		/// </summary>
+		/// <typeparam name="T">Array element type.</typeparam>
+		/// <param name="array">Array to shuffle.</param>
+		public static T[] ShuffleArray<T>(T[] array)
+		{
+		
+			for (int i = array.Length; i > 1; i--)
+			{
+				// Pick random element to swap.
+				int j = Random.Range(0, i); // 0 <= j <= i-1
+				// Swap.
+				T tmp = array[j];
+				array[j] = array[i - 1];
+				array[i - 1] = tmp;
+			}
+			return array;
+		}
+	}
+
+}
